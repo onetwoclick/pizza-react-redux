6d0406b88658a25bd22740eef6921dd75117e402
onetwoclick <i.ivanov@internet.ru>
2022-03-29 10:52:40 +0300

    redux hooks

--- src/App.js
+++ src/App.js
@@ -1,84 +1,77 @@
 import React from "react";
-import { Route, Routes } from 'react-router-dom'
+import {Route, Routes} from 'react-router-dom'
 import axios from "axios";
 import {Header} from "./components";
 import {Home, Cart} from "./pages";
 import {setPizzas} from "./redux/actions/pizzas";
-import {connect} from "react-redux";
+import {useDispatch} from "react-redux";
 
-// function App() {
-//   React.useEffect(() => {
-//     axios.get('http://localhost:3000/db.json').then(({ data }) =>{
-//       store.dispatch(setPizzas(data.pizzas));
-//     });
-//
-//     // fetch('http://localhost:3002/db.json')
-//     //   .then((resp) => resp.json())
-//     //   .then((json) => {
-//     //     setPizzas(json.pizzas);
-//     //   })
-//   },[]);
-//
-//   return (
-//     <div className="wrapper">
-//       <Header />
-//       <div className="content">
-//           <Routes>
-//               <Route
-//                 path="/"
-//                 element={
-//                   <Home items={pizzas}/>
-//                 }
-//               />
-//               <Route
-//                 part="/cart"
-//                 element={
-//                   <Cart />
-//                 }
-//               />
-//           </Routes>
-//       </div>
-//     </div>
-//   );
-// }
+function App() {
+  const dispatch = useDispatch();
 
-class App extends React.Component{
-  componentDidMount() {
-    axios.get('http://localhost:3000/db.json').then(({ data }) =>{
-      this.props.dispatch(setPizzas(data.pizzas));
+  React.useEffect(() => {
+    axios.get('http://localhost:3001/db.json').then(({data}) => {
+      dispatch(setPizzas(data.pizzas));
     });
-  }
+    // fetch('http://localhost:3002/db.json')
+    //   .then((resp) => resp.json())
+    //   .then((json) => {
+    //     setPizzas(json.pizzas);
+    //   })
+  }, []);
 
-  render() {
-    console.log(this.props)
-    return(
-      <div className="wrapper">
-      <Header />
+  return (
+    <div className="wrapper">
+      <Header/>
       <div className="content">
         <Routes>
-          <Route
-            path="/"
-            element={
-              <Home items={this.props.items}/>
-            }
-          />
-          <Route
-            part="/cart"
-            element={
-              <Cart />
-            }
-          />
+          <Route path="/" element={<Home/>} />
+          <Route part="/cart" element={<Cart/>} />
         </Routes>
       </div>
     </div>
-    )
-  }
+  );
 }
 
-const mapStateToProps = (state) => {
-  return{
-    items: state.pizzas.items,
-  }
-}
+// class App extends React.Component{
+//   componentDidMount() {
+//     axios.get('http://localhost:3000/db.json').then(({ data }) =>{
+//       this.props.dispatch(setPizzas(data.pizzas));
+//     });
+//   }
+//
+//   render() {
+//     console.log(this.props)
+//     return(
+//       <div className="wrapper">
+//       <Header />
+//       <div className="content">
+//         <Routes>
+//           <Route
+//             path="/"
+//             element={
+//               <Home items={this.props.items}/>
+//             }
+//           />
+//           <Route
+//             part="/cart"
+//             element={
+//               <Cart />
+//             }
+//           />
+//         </Routes>
+//       </div>
+//     </div>
+//     )
+//   }
+// }
 
-export default connect(mapStateToProps)(App);
+// const mapStateToProps = (state) => {
+//   return{
+//     items: state.pizzas.items,
+//   }
+// }
+//
+// export default connect(mapStateToProps)(App);
+
+export default App;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,37 +1,46 @@
 import {Categories, PizzaCard, SortPopup} from "../components";
+import {useDispatch, useSelector} from "react-redux";
+import {setCategory} from "../redux/actions/filters";
 
-const categories = ['Мясные','Вегетарианская','Гриль','Острые','Закрытые'];
+const categories = ['Мясные', 'Вегетарианская', 'Гриль', 'Острые', 'Закрытые'];
 const sort = [
   {name: 'популярности', type: 'popular'},
   {name: 'цене', type: 'price'},
   {name: 'алфавиту', type: 'alphabet'},
 ]
 
-function Home({items}){
-    return(
-        <div className="container">
-            <div className="content__top">
-                <Categories
-                    items={categories}
-                    onClickItem={(name) => console.log(name)}
-                />
-                <SortPopup
-                    items={sort}
-                />
-            </div>
-            <h2 className="content__title">Все пиццы</h2>
-            <div className="content__items">
-              {
-                items.map(obj => (
-                  <PizzaCard
-                    key={obj.id}
-                    {...obj}
-                  />
-                ))
-              }
-            </div>
-        </div>
-    )
+function Home() {
+  const dispatch = useDispatch();
+  const items = useSelector(({pizzas}) => pizzas.items);
+
+  const onSelectCategory = index => {
+    dispatch(setCategory(index));
+  }
+
+  return (
+    <div className="container">
+      <div className="content__top">
+        <Categories
+          items={categories}
+          onClickItem={onSelectCategory}
+        />
+        <SortPopup
+          items={sort}
+        />
+      </div>
+      <h2 className="content__title">Все пиццы</h2>
+      <div className="content__items">
+        {items &&
+        items.map(obj => (
+          <PizzaCard
+            key={obj.id}
+            {...obj}
+          />
+        ))
+        }
+      </div>
+    </div>
+  )
 }
 
 export default Home;
